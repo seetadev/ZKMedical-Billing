23efd03f94cfdd59bb28971620606dc982078d89
Mukul Kolpe <mukulkolpe45@gmail.com>
2024-06-26 02:59:59 +0530

    Integrated token gated doctor registration form

---
+++ MedETH/frontend/src/pages/doctor-registration/index.tsx
@@ -0,0 +1,13 @@
+// @ts-nocheck comment
+import React from "react";
+import MultiStep from "../../components/DoctorRegistrationForm/Form";
+
+const DoctorRegistration = () => {
+  return (
+    <div>
+      <MultiStep />
+    </div>
+  );
+};
+
+export default DoctorRegistration;
